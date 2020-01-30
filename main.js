2b6b550801e3d7f96ecebaaeeabac252fcc5470b
Nathan Bastard <nathan.bastardpfeg@gmail.com>
2020-01-30 18:42:00 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,34 @@
+var cardList 	= $('.cardList').first(), 
+    cards 		= $('.card'), 
+    speed 		= 1000,
+    width 		= 100, 
+    randomize 	= true, 
+    distance 	= 20 * width 
+;
+
+for (var i = 0; i < 50; i++) {
+    cards.clone().appendTo(cardList);
+}
+
+function spin() {
+    var newMargin = 0, newDistance = distance;
+    if (randomize) {
+        newDistance = Math.floor(Math.random() * cards.length * 5);
+		    newDistance += cards.length * 10;
+        var rand = Math.floor((Math.random()*100)+1);
+        newDistance *= rand;
+    } 
+	newMargin = -(newDistance);
+    cards.first().animate({
+        marginLeft: newMargin
+    }, 7500);
+}
+
+$('#spin').click(function() {
+    //cards.first().css('margin-left', 0);
+    setTimeout(spin,500);
+    return false;
+});
+
+
+
